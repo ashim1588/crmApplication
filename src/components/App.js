c1c687e9b515eb342739566c6f4796abdc9e38ca
ashim1588 <ashimmehar158@gmail.com>
2023-11-25 21:57:03 +0530

    BottomsTabBar

--- src/components/App.js
+++ src/components/App.js
@@ -10,46 +10,20 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import reducers from '../reducers/PeopleReducer';
-import PeopleList from './PeopleList';
+import TabNavigator from './Navigation';
 
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-const Tab = createBottomTabNavigator();
-
-function Dummy() {
-  return (
-    <View>
-      <Text>Dummy</Text>
-    </View>
-  );
-}
-
-function TabNavigation() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="home">
-        <Tab.Screen name="home" component={Dummy} />
-        <Tab.Screen name="auth" component={Dummy} />
-        <Tab.Screen name="bucket" component={Dummy} />
-        <Tab.Screen name="menu" component={Dummy} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <NavigationContainer>
-          <TabNavigation />
-        </NavigationContainer>
+        <TabNavigator />
       </Provider>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,31 @@
-import PeopleList from './PeopleList';
-import CompanyList from './CompanyList';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import PeopleList from './PeopleLIst';
 import AddPerson from './AddPerson';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import CompanyList from './CompanyList';
 
 const Tab = createBottomTabNavigator();
+
 function TabNavigator() {
   return (
-    <Tab.Navigator >
-      <Tab.Screen name="People" component={PeopleList} />
-      <Tab.Screen name="Add" component={AddPerson} />
-      <Tab.Screen name="Company" component={CompanyList} />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="People"
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: '#80cbc4',
+          showLabel: true,
+          style: {
+            backgroundColor: '#26a69a',
+          },
+        }}>
+        <Tab.Screen name="People" component={PeopleList} />
+        <Tab.Screen name="Add" component={AddPerson} />
+        <Tab.Screen name="Company" component={CompanyList} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-//   {
-//     initialRouteName: 'People',
-//     tabBarOptions: {
-//       activeTintColor: 'white',
-//       inactiveTintColor: '#80cbc4',
-//       showLabel: false,
-//       showIcon: true,
-//       style: {
-//         backgroundColor: '#26a69a',
-//       },
-//     },
-//   },
-// );
 
 export default TabNavigator;
